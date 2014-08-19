5a86aef7da6a181e179cd0cab655f11649fbd825
Nicolas <nicomiranda@gmail.com>
2014-08-19 15:47:58 -0400

    working on linear svm

--- Nicolas/linearsvc_test.py
+++ Nicolas/linearsvc_test.py
@@ -1,4 +1,6 @@
 from sklearn import svm
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'GalaxyZooData.csv')
+file_path = os.path.join(os.path.dirname(__file__), 'data_csv', os.pardir, 'GalaxyZooData.csv')
+data_svm = np.genfromtxt(file_path, delimiter=',', names=True).transpose()
+clf = svm.LinearSVC()
