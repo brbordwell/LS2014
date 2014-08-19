c713747fd96ee3b53e5dea501737730822657d3f
Nicolas <nicomiranda@gmail.com>
2014-08-19 14:19:19 -0400

    visualization test path corrected

--- Nicolas/data_visual_test.py
+++ Nicolas/data_visual_test.py
@@ -1,11 +1,13 @@
+import os
 import numpy as np
 import pylab as pl
 
-data = np.genfromtxt("/home/nico/workspace/LS2014/Data/GalaxyZooTraining_CherenkovRad.csv", delimiter=',', names=True)
+file_path = os.path.join(os.path.dirname(__file__), os.pardir, 'Data', 'GalaxyZooTraining_CherenkovRad.csv')
+data = np.genfromtxt(file_path, delimiter=',', names=True)
 eliptical, spiral, u, g, i, z = data['p_el_debiased'] > .8, data['p_cs_debiased'] > .8, data['u'], data['g'], data['i'], data['z']
 unknown = np.ones(len(eliptical), dtype=bool) & (~eliptical) & (~spiral)
-colors = u - g, i - z
-names = np.array(["u - g", "i - z"])
+colors = u - i, i
+names = np.array(["u - i", "i"])
 cl_names = ["eliptical", 'spiral', 'unknown']
 pl.figure(figsize = (10, 10))
 pl.plot(colors[0][eliptical], colors[1][eliptical], "o", markersize=5, label = cl_names[0])
