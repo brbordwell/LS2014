a0814571625a7d2888558f5cfa776b1adc87777b
Nicolas <nicomiranda@gmail.com>
2014-08-20 09:36:26 -0400

    Added iterating process to get median and standard deviation

--- Nicolas/linearsvc_test.py
+++ Nicolas/linearsvc_test.py
@@ -13,7 +13,7 @@ file_path = os.path.join(os.path.dirname(__file__), os.pardir, 'data_csv', 'Gala
 
 #file_path = os.path.join(os.path.dirname(__file__), os.pardir, 'data_csv', 'GalaxyZooData_2.csv')
 with open(file_path) as t_in:
-	data = np.genfromtxt(itertools.islice(t_in, 50001), delimiter=',', names=True).transpose()
+	data = np.genfromtxt(itertools.islice(t_in, 10001), delimiter=',', names=True).transpose()
 
 # Generate a vector of classes
 eliptical, spiral = data['p_el_debiased'] > .8, data['p_cs_debiased'] > .8
@@ -28,13 +28,18 @@ data_svm = data[svn_names]
 data_svm = data_svm.view(np.float64).reshape(data_svm.shape + (-1,))
 
 clf = svm.LinearSVC()
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(data_svm, classes, test_size=1./3.)
-print "training set = ", X_train.shape, y_train.shape
-print "test size = ", X_test.shape, y_test.shape
-clf.fit(X_train, y_train)
-print clf
-pred_class = clf.predict(X_test)
-N_match = (pred_class == y_test).sum()
-print "N_match = ", N_match
-acc = 1. * N_match / len(pred_class)
-print "Accuracy = ", acc
+
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(data_svm, classes, test_size=1./3.)
+#print "training set = ", X_train.shape, y_train.shape
+#print "test size = ", X_test.shape, y_test.shape
+#clf.fit(X_train, y_train)
+#print clf
+#pred_class = clf.predict(X_test)
+#N_match = (pred_class == y_test).sum()
+#print "N_match = ", N_match
+#acc = 1. * N_match / len(pred_class)
+#print "Accuracy = ", acc
+
+ss = cross_validation.StratifiedShuffleSplit(classes, n_iter=10, test_size=1./3.)
+scores = cross_validation.cross_val_score(clf, data_svm, classes, cv=ss)
+print "Accuracy = ", scores.mean(), "+-", scores.std()
